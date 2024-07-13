ad792a92edf3258dafa8a013248ea7526efd93b6
sonny <dhsmf4245@gmail.com>
2024-07-13 15:50:15 +0900

    백준 1324

--- backjun/Silver_1324.java
+++ backjun/Silver_1324.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class Silver_1324 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int t = sc.nextInt();;
     }
 }
