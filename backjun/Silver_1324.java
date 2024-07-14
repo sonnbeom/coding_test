e3e8e4ea27002971648cffc14eac8ff7e700cc06
sonny <dhsmf4245@gmail.com>
2024-07-14 09:01:20 +0900

    백준 1324

--- backjun/Silver_1324.java
+++ backjun/Silver_1324.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class Silver_1324 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();;
+        int t = sc.nextInt();
+        int[] arr = new int[t];
+        for (int i = 0; i < t; i++) {
+            arr[i] = sc.nextInt();
+        }
     }
 }
